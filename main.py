e8f1eb613ae8188221712504991072009b05bcdb
LuukNeervens <luukneervens2002@gmail.com>
2024-11-10 19:37:14 +0100

    Organized everything and added scenario test code

--- main.py
+++ main.py
@@ -2,24 +2,83 @@ import stk.python27bridge
 import stk.events
 import stk.services
 import dances
+import speechrec
+import time as t
+from transformers import pipeline
+import librosa
+import threading
+import os
+# os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+# import tensorflow 
 
-class Python3NaoExample:
+class NaoDanceTutor:
+    """ Main Nao class, from here all other classes are instantiated. """
+    
     def __init__(self):
         # Bridge
         self.python27bridge = stk.python27bridge.Python27Bridge()
         self.events = stk.events.EventHelper(self.python27bridge)
         self.s = stk.services.ServiceCache(self.python27bridge)
-        # Test speech
-        self.s.ALTextToSpeech.say("Luuk has beautiful eyes")
-        # Import from dances file
-        Dances = dances.Dances()
 
-        dance = Dances.air_guitar(multiplier=0.5)
+        # Initialize class instances
+        self.dances = dances.Dances()
+        self.speechrec = speechrec.SpeechRecognition(self.s)
+
+    def say(self, message):
+        try:
+            self.s.ALTextToSpeech.say(message)
+        except Exception as e:
+            print(f"Error in say_message: {e}")
+        
+    def test_dance(self):
+        dance = self.dances.air_guitar(multiplier=0.5)
         self.s.ALMotion.angleInterpolationBezier(dance[0], dance[1], dance[2])
 
-        dance = Dances.dance_move()
+        dance = self.dances.dance_move()
         self.s.ALMotion.angleInterpolationBezier(dance[0], dance[1], dance[2])
+
+    def motion_detected(self):
+        """ IMPLEMENT """
+        return True
+    
+    def teach_move(self):
+        """ IMPLEMENT """
+        self.test_dance()
+    
+    def dance_together(self):
+        """ IMPLEMENT """
+        # Somehow need to get both commands simulatenous, using the .post function, how?
+        self.s.ALAudioPlayer.playFile(os.path.join(os.getcwd(), "NaoBeat.wav").replace("\\", "/"))
+        self.test_dance()
+
+    
+    def introduction(self):
+        if self.motion_detected():
+            self.say("Hi there!")
+        t.sleep(2)                  # give time for response
+        #name = nao.whispermini(3.0)['text']
+
+        self.say("My name is Nao, I am here to teach you some cool moves, but most importantly: to have fun together!")
+        self.say("First off, you can choose whether you want to learn a dancemove, or to just dance together. What would you prefer?")
+        
+    
+    def scenario(self):
+        #input = self.speechrec.whispermini(3.0)['text']
+        input = "I want to dance"
+        if "learn" in input:
+            self.teach_move()
+        else:
+            self.dance_together()
+    
+    def main(self):
+        self.introduction()
+        self.scenario()
+
+        
 
     
 if __name__ == "__main__":
-    Python3NaoExample()
+    nao = NaoDanceTutor()
+    nao.main()
+
+
---
+++ speechrec.py
@@ -0,0 +1,63 @@
+import time as t
+import speech_recognition as sr
+from transformers import pipeline
+import librosa
+
+
+class SpeechRecognition:
+    DEVICE = "cpu" 
+    MODEL_NAME = "openai/whisper-tiny.en"  
+
+    def __init__(self, s):
+        self.r = sr.Recognizer()    
+        self.r.energy_threshold = 4000
+        self.s = s
+
+        self.pipe = pipeline(
+			task="automatic-speech-recognition",
+			model=self.MODEL_NAME,
+			# language="en",
+			chunk_length_s=2,
+			device=self.DEVICE,
+		)
+
+        self.pipe.model.config.forced_decoder_ids = self.pipe.tokenizer.get_decoder_prompt_ids(language="en", task="transcribe")
+    
+    def speech_recognize(self, time): 
+        with sr.Microphone() as source:
+            one = t.time()
+            self.r.adjust_for_ambient_noise(source)
+            two = t.time()
+            print("mic on... start recording!")
+            audio = self.r.listen(source, phrase_time_limit=time)
+            print(audio.get_raw_data())
+            print("done recording")
+            three = t.time()
+
+			# with open(r'C:\Users\thoma\Documents\Studie\M1\HRI\bridge\robot-jumpstarter-python3-master\python3\test.wav', 'wb') as f:
+			# 	f.write(audio.get_wav_data())
+            words = self.r.recognize_whisper(audio)
+            four = t.time()
+            print('Reconized words: ' + words)
+            print("2-1: ", two - one )
+            print("3-2: ", three - two )
+            print("4-3: ", four - three )
+            print("total time: ", four - one )
+			
+            return words
+        
+    def whispermini(self, time):
+        with sr.Microphone() as source:
+            self.r.adjust_for_ambient_noise(source)
+            self.s.ALAudioPlayer.playSine(1000, 10, 1.0, 0.5)
+            audio = self.r.listen(source, phrase_time_limit=time)
+            self.s.ALAudioPlayer.playSine(2000, 10, 1.0, 0.5)
+
+            with open("microphone-results.wav", "wb") as f:
+                f.write(audio.get_wav_data())
+
+            wav, rate = librosa.load("microphone-results.wav")
+            wav = librosa.resample(wav, orig_sr=rate, target_sr=16000)
+
+            words = self.pipe(wav, batch_size=32)
+            return words
