73eb760a8f406e42c7b271100c594462b1838282
Thomas Helsen <thomas.helsen@ru.nl>
2024-11-28 12:04:46 +0100

    Other merge

--- pose_detection/human_pose_detection.py
+++ pose_detection/human_pose_detection.py
@@ -96,6 +96,7 @@ class PoseDetector:
             print("Camera opened")
 
     def take_pics(self, sleep_time = 0.5):
+        # TODO: fix sleep time (takes only one picture and saves it 3 times or smth)
         if self.verbose:
             print("Start taking pictures")
         while True:
@@ -412,6 +413,7 @@ class PoseDetector:
                 counter = self.process_frame(frame1, frame2, threshold, incremental, counter)
 
                 if counter is True: 
+                    print('Motion detected!')
                     return True
 
                 # Update frames
@@ -423,6 +425,7 @@ class PoseDetector:
                 counter = self.process_frame(frame1, frame2, threshold, incremental, counter)
 
                 if counter is True:  # counter is either int or bool
+                    print('Motion detected!')
                     return True
 
                 # Update frames
